1384955708556ebfef756b0195430aaf4902ef84
Harikrishna <harikrishna.sree@outlook.com>
2024-02-07 13:38:53 +0530

    05 FEB 2024 --> Assignments

---
+++ 05 FEB 2023/Print the tables.py
@@ -0,0 +1,4 @@
+num=int(input("Enter any number is:- "))
+for a in range(1, 11):
+    print(num,'X', a,'=', num*a)
+print("thank you")
---
+++ 05 FEB 2023/Print the user details.py
@@ -0,0 +1,9 @@
+#Take a user input (name, age, roll_number, phone_number) and print the user details
+name=input("Enter Your name is:-")
+age=input("Enter You Age is:-")
+roll_number=input("Enter Your roll_number is:- ")
+phone_number=input("Enter Your Phone_number is:-")
+print("Name; ", name)
+print("Age: ", age)
+print("Roll Number: ", roll_number)
+print("Phone Number: ",phone_number)
